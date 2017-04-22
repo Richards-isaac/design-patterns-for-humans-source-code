fc9eb23212c655520f3592e8305570912d087e49
sydorenkovd <sydorenkovd@gmail.com>
2017-04-22 23:09:35 +0300

    builder

---
+++ patterns/builder.php
@@ -0,0 +1,81 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: sydorenkovd
+ * Date: 22.04.17
+ * Time: 23:03
+ */
+class Burger
+{
+    protected $size;
+
+    protected $cheese = false;
+    protected $pepperoni = false;
+    protected $lettuce = false;
+    protected $tomato = false;
+
+    public function __construct(BurgerBuilder $builder)
+    {
+        $this->size = $builder->size;
+        $this->cheese = $builder->cheese;
+        $this->pepperoni = $builder->pepperoni;
+        $this->lettuce = $builder->lettuce;
+        $this->tomato = $builder->tomato;
+    }
+    public function testClass() {
+        echo $this->cheese . $this->pepperoni ."(".$this->size.")";
+    }
+}
+
+class BurgerBuilder
+{
+    public $size;
+
+    public $cheese = false;
+    public $pepperoni = false;
+    public $lettuce = false;
+    public $tomato = false;
+
+    public function __construct(int $size)
+    {
+        $this->size = $size;
+    }
+
+    public function addPepperoni()
+    {
+        $this->pepperoni = true;
+        return $this;
+    }
+
+    public function addLettuce()
+    {
+        $this->lettuce = true;
+        return $this;
+    }
+
+    public function addCheese()
+    {
+        $this->cheese = true;
+        return $this;
+    }
+
+    public function addTomato()
+    {
+        $this->tomato = true;
+        return $this;
+    }
+
+    public function build(): Burger
+    {
+        return new Burger($this);
+    }
+}
+
+$burger = (new BurgerBuilder(14))
+    ->addPepperoni()
+    ->addLettuce()
+    ->addTomato()
+    ->addCheese()
+    ->build();
+$burger->testClass();
