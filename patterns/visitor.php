74fb74804b10f5497c5f14bff18ff527b42e48a7
sydorenkovd <sydorenkovd@gmail.com>
2017-09-09 18:00:04 +0300

    visitor

---
+++ patterns/visitor.php
@@ -0,0 +1,85 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: sydorenkovd
+ * Date: 09.09.17
+ * Time: 17:53
+ */
+// Место посещения
+interface Animal
+{
+    public function accept(AnimalOperation $operation);
+}
+
+// Посетитель
+interface AnimalOperation
+{
+    public function visitMonkey(Monkey $monkey);
+    public function visitLion(Lion $lion);
+    public function visitDolphin(Dolphin $dolphin);
+}
+
+class Monkey implements Animal
+{
+    public function shout()
+    {
+        echo 'Ooh oo aa aa!';
+    }
+
+    public function accept(AnimalOperation $operation)
+    {
+        $operation->visitMonkey($this);
+    }
+}
+
+class Lion implements Animal
+{
+    public function roar()
+    {
+        echo 'Roaaar!';
+    }
+
+    public function accept(AnimalOperation $operation)
+    {
+        $operation->visitLion($this);
+    }
+}
+
+class Dolphin implements Animal
+{
+    public function speak()
+    {
+        echo 'Tuut tuttu tuutt!';
+    }
+
+    public function accept(AnimalOperation $operation)
+    {
+        $operation->visitDolphin($this);
+    }
+}
+class Speak implements AnimalOperation
+{
+    public function visitMonkey(Monkey $monkey)
+    {
+        $monkey->shout();
+    }
+
+    public function visitLion(Lion $lion)
+    {
+        $lion->roar();
+    }
+
+    public function visitDolphin(Dolphin $dolphin)
+    {
+        $dolphin->speak();
+    }
+}
+$monkey = new Monkey();
+$lion = new Lion();
+$dolphin = new Dolphin();
+
+$speak = new Speak();
+
+$monkey->accept($speak);    // Уа-уа-уааааа!
+$lion->accept($speak);      // Ррррррррр!
+$dolphin->accept($speak);   // Туут тутт туутт!
